471c4d7a0c3032666a5e7d44f20908a5cfebc674
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-05-30 16:07:19 +0900

    Add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import ModeToggle from '@/components/ModeToggle';
 import { Button } from '@/components/ui/button';
 import {
   SignedIn,
@@ -11,18 +10,7 @@ import {
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
-      <Button variant={'secondary'}>Click me</Button>
+      <h1>home page content</h1>
     </div>
   );
 }
