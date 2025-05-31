bdeee4036a4c22cf1370b0918b31d524bfb374f5
Takayuki Kitamoto <t.kitamoto@mailfence.com>
2025-05-31 16:11:10 +0900

    Add CreatePost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import { Button } from '@/components/ui/button';
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from '@clerk/nextjs';
+import CreatePost from '@/components/CreatePost';
+import { currentUser } from '@clerk/nextjs/server';
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>home page content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+
+      <div className="hideen lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
     </div>
   );
 }
